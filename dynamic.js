d3b6ed6fc23bf2efa6a812ff59f42a3b4c523925
Paul Ebose <paulebose@gmail.com>
2020-11-10 10:39:57 +0100

    feat: show confirmation when form is submitted

--- dynamic.js
+++ dynamic.js
@@ -1,1 +1,22 @@
-const 
+window.addEventListener('load', (ev) => {
+  const form = document.forms.item(0)
+  const submitted = document.getElementById('submitted')
+
+  form.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+
+    const firstName = document.getElementById('firstName')
+    const lastName = document.getElementById('lastName')
+    const email = document.getElementById('email')
+
+    console.log('submitted!', {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+    })
+
+    form.style.display = 'none'
+
+    submitted.style.display = 'block'
+  })
+})
